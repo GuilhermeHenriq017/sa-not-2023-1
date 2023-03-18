a66f6c8d28b64619839f5d8da313c62e1e300857
Guilherme Henrique Carvalho <79169104+GuilhermeHenriq017@users.noreply.github.com>
2023-03-18 00:32:20 +0000

    17/03 auth-session

---
+++ auth/controllers/users.js
@@ -0,0 +1,12 @@
+const conn = require('../config/database')
+
+const controller = {}
+
+    controller.formNew = (req, res) => {
+    res.render('user_form', {
+        title: 'Cadastrar novo usuário'
+    })
+
+}
+
+module.exports = controller
