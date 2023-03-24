44c3228f46bf05c62d4cf116c449517ef98a202a
Guilherme Henrique Carvalho <79169104+GuilhermeHenriq017@users.noreply.github.com>
2023-03-24 23:24:25 +0000

    24/03 - Auth-session

--- auth/routes/users.js
+++ auth/routes/users.js
@@ -4,5 +4,10 @@ const controller = require('../controllers/users')
 
 /* GET users listing. */
 router.get('/form', controller.formNew) 
+router.get('/login', controller.formLogin) 
+router.post('/', controller.create) 
+router.post('/auth', controller.auth) 
+
+
 
 module.exports = router;
